61735ec4fd0202a605ff58034ff2d0a0c9a417b7
makethumb <sourabhs701@gmail.com>
2025-09-22 15:23:20 +0530

    config: github_removed

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -10,7 +10,7 @@ export function SiteFooter() {
     <footer className="max-w-screen overflow-x-hidden px-2">
       <div className="screen-line-before mx-auto border-x border-edge pt-4 md:max-w-3xl">
         <p className="mb-1 px-4 text-center font-mono text-sm text-balance text-muted-foreground">
-          Inspired by chanhdai.com & magicui.design
+          Inspired by chanhdai & magicui.design
         </p>
 
         <p className="mb-4 px-4 text-center font-mono text-sm text-balance text-muted-foreground">
@@ -23,7 +23,7 @@ export function SiteFooter() {
           >
             sourabhs701
           </a>
-          . The source code is available on{" "}
+          {/* . The source code is available on{" "}
           <a
             className="link"
             href={SOURCE_CODE_GITHUB_URL}
@@ -32,7 +32,7 @@ export function SiteFooter() {
           >
             GitHub
           </a>
-          .
+          . */}
         </p>
 
         <div
